464540509c448e79b1bcca7af677f474e9ea2c4e
Shreyansh Pande <179868648+shreyanshVIT23@users.noreply.github.com>
2026-06-26 22:32:19 +0530

    Completed - Implement services for managing Country in Spring Data JPA Handson

--- Week 2/Spring Data JPA Handson/scripts/initialize_table.sql
+++ Week 2/Spring Data JPA Handson/scripts/initialize_table.sql
@@ -1,13 +1,11 @@
 DROP TABLE IF EXISTS country;
 
--- 4. Create the country table
 CREATE TABLE country (
     code VARCHAR(2) NOT NULL,
     name VARCHAR(50) NOT NULL,
     PRIMARY KEY (code)
 );
 
--- 5. Insert sample records
 INSERT INTO country (code, name)
 VALUES
 ('IN', 'India'),
@@ -21,8 +19,6 @@ VALUES
 ('CN', 'China'),
 ('BR', 'Brazil');
 
--- 6. Verify the data
 SELECT * FROM country;
 
--- 7. Display table structure
 DESCRIBE country;
