472d293aae5767e7c1b79d985dd2dca0d3a10382
Shreyansh Pande <179868648+shreyanshVIT23@users.noreply.github.com>
2026-07-03 00:23:54 +0530

    Completed - REST - Get country based on country code in Spring REST using Spring Boot 3

--- Week 3/Spring REST using Spring Boot 3/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
+++ Week 3/Spring REST using Spring Boot 3/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -1,9 +1,13 @@
 package com.cognizant.spring_learn.controller;
 
+import com.cognizant.spring_learn.service.CountryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +18,9 @@ public class CountryController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
 
+    @Autowired
+    private CountryService countryService;
+
     @RequestMapping("/country")
     public Country getCountryIndia() {
         LOGGER.info("START getCountryIndia");
@@ -21,6 +28,15 @@ public class CountryController {
         Country country = context.getBean("country", Country.class);
         LOGGER.debug("Country : {}", country);
         LOGGER.info("END getCountryIndia");
+        return country;
+    }
+
+    @GetMapping("/country/{code}")
+    public Country getCountry(@PathVariable String code) {
+        LOGGER.info("START getCountry");
+        Country country = countryService.getCountry(code);
+        LOGGER.debug("Country : {}", country);
+        LOGGER.info("END getCountry");
         return country;
     }
 }
