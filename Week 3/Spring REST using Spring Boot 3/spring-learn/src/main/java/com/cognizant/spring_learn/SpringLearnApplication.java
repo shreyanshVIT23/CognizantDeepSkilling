bded7c687e89fb8f98377d9634cd8625323f53d5
Shreyansh Pande <179868648+shreyanshVIT23@users.noreply.github.com>
2026-07-02 23:36:26 +0530

    Completed - Incorporate Logging in Spring REST using Spring Boot 3

--- Week 3/Spring REST using Spring Boot 3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week 3/Spring REST using Spring Boot 3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,5 +1,7 @@
 package com.cognizant.spring_learn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,6 +12,7 @@ import java.util.Date;
 
 @SpringBootApplication
 public class SpringLearnApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringLearnApplication.class, args);
@@ -17,10 +20,13 @@ public class SpringLearnApplication {
 	}
 
 	public static void displayDate() throws Exception {
-		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		LOGGER.info("START");
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("date-format.xml");
 		SimpleDateFormat format =
 				context.getBean("dateFormat", SimpleDateFormat.class);
 		Date date = format.parse("31/12/2018");
-		System.out.println(date);
+		LOGGER.debug("Parsed Date: {}", date);
+		LOGGER.info("END");
 	}
 }
