986d849b9c0c12090155a7c0a69a553a57f5c205
Shreyansh Pande <179868648+shreyanshVIT23@users.noreply.github.com>
2026-07-16 14:10:07 +0530

    Completed Task 3 on Angular HandsOn 2

--- Week 5/Angular_HandsON/ShreyanshPande/student-course-portal/src/app/components/course-card/course-card.ts
+++ Week 5/Angular_HandsON/ShreyanshPande/student-course-portal/src/app/components/course-card/course-card.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-course-card',
@@ -14,5 +14,12 @@ export class CourseCard implements OnChanges {
     );
   }
   @Input()
-  course: any;
+  course!: {
+    id: number;
+    name: string;
+    code: string;
+    credits: number;
+  } ;
+  @Output()
+  enrollRequested = new EventEmitter<number>();
 }
--- Week 5/Angular_HandsON/ShreyanshPande/student-course-portal/src/app/pages/course-list/course-list.ts
+++ Week 5/Angular_HandsON/ShreyanshPande/student-course-portal/src/app/pages/course-list/course-list.ts
@@ -7,4 +7,48 @@ import { CourseCard } from '../../components/course-card/course-card';
   templateUrl: './course-list.html',
   styleUrl: './course-list.css',
 })
-export class CourseList {}
+export class CourseList {
+  courses = [
+    {
+      id: 1,
+      name: 'Angular',
+      code: 'CS301',
+      credits: 4,
+    },
+
+    {
+      id: 2,
+      name: 'Python',
+      code: 'CS302',
+      credits: 3,
+    },
+
+    {
+      id: 3,
+      name: 'Machine Learning',
+      code: 'CS401',
+      credits: 5,
+    },
+
+    {
+      id: 4,
+      name: 'Database Systems',
+      code: 'CS205',
+      credits: 4,
+    },
+
+    {
+      id: 5,
+      name: 'Operating Systems',
+      code: 'CS303',
+      credits: 4,
+    },
+  ];
+  selectedCourseId: number | null = null;
+
+  onEnroll(courseId: number) {
+    console.log('Enrolling in course: ' + courseId);
+
+    this.selectedCourseId = courseId;
+  }
+}
