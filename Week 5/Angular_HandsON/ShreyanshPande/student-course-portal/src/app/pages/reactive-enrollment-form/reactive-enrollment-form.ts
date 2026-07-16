316c97b07a09706f87a44115c5b576a6b04acf83
Shreyansh Pande <179868648+shreyanshVIT23@users.noreply.github.com>
2026-07-17 00:36:55 +0530

    Completed Task 2 on Angular HandsOn 5

--- Week 5/Angular_HandsON/ShreyanshPande/student-course-portal/src/app/pages/reactive-enrollment-form/reactive-enrollment-form.ts
+++ Week 5/Angular_HandsON/ShreyanshPande/student-course-portal/src/app/pages/reactive-enrollment-form/reactive-enrollment-form.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ValidationErrors,
+  AbstractControl,
+  FormArray,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-enrollment-form',
@@ -14,14 +23,49 @@ export class ReactiveEnrollmentForm implements OnInit {
   ngOnInit(): void {
     this.enrollForm = this.fb.group({
       studentName: ['', [Validators.required, Validators.minLength(3)]],
-      studentEmail: ['', [Validators.required, Validators.email]],
-      courseId: [null, Validators.required],
+      studentEmail: [
+        '',
+        [Validators.required, Validators.email],
+        [this.simulateEmailCheck.bind(this)],
+      ],
+      courseId: ['', [Validators.required, this.noCourseCode.bind(this)]],
       preferredSemester: ['Odd', Validators.required],
       agreeToTerms: [false, Validators.requiredTrue],
+      additionalCourses: this.fb.array([]),
     });
   }
   onSubmit() {
     console.log('value:', this.enrollForm.value);
     console.log('raw value:', this.enrollForm.getRawValue());
+  }
+  noCourseCode(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    if (value && value.toString().startsWith('XX')) {
+      return { noCourseCode: true };
+    }
+    return null;
+  }
+  simulateEmailCheck(control: AbstractControl): Promise<ValidationErrors | null> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const email = control.value;
+        if (email && email.includes('test@')) {
+          resolve({
+            emailTaken: true,
+          });
+        } else {
+          resolve(null);
+        }
+      }, 800);
+    });
+  }
+  get additionalCourses(): FormArray<FormControl<string | null>> {
+    return this.enrollForm.get('additionalCourses') as FormArray<FormControl<string | null>>;
+  }
+  addCourse() {
+    this.additionalCourses.push(this.fb.control('', Validators.required));
+  }
+  removeCourse(index: number) {
+    this.additionalCourses.removeAt(index);
   }
 }
